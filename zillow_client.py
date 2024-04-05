81719cc30b46d74ace864cf6b5c9558aedb6ce5d
dbeyer03 <dbeyer03@bu.edu>
2024-04-05 08:40:00 -0400

    MINOR EDIT: changed response to response.json

--- zillow_client.py
+++ zillow_client.py
@@ -27,7 +27,7 @@ def get_reviews(self,iUrl, iSkip,iSelect,iUnselect,iTop,iOrderby):
   response = requests.request("GET",bridge_url, params=bridge_params)
   if response.ok():
     print(response.text)
-    return response
+    return response.json()
   else:
     response.raise_for_status()
 """
