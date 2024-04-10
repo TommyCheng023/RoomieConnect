eefaa7fab9a79b0f2cb89825b6db4342f64aa836
dbeyer03 <dbeyer03@bu.edu>
2024-04-10 17:01:55 -0400

    Please. commit.

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+class Config:
+    DEBUG = False
+    SECRET_KEY = ''
 class Config(object):
     DEBUG = False
     DATABASE_HOST = '127.0.0.1'
     DATABASE_USER = 'root'
     DATABASE_PASSWORD = ''
     DATABASE_DB = 'RoomieConnect'
-class Config:
-    DEBUG = False
-    SECRET_KEY = ''
+
